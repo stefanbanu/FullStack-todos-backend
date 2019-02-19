f2f00dfc5dac20c41bd56532d74f3b723c7d1c3a
Stefan Banu <stefanbanu1@gmail.com>
2019-02-19 05:02:06 +0200

    Fixed cross-origin for TodoResource controller.

--- src/main/java/com/stefanbanu/todowebservices/controllers/TodoResource.java
+++ src/main/java/com/stefanbanu/todowebservices/controllers/TodoResource.java
@@ -3,6 +3,7 @@ package com.stefanbanu.todowebservices.controllers;
 import com.stefanbanu.todowebservices.dto.Todo;
 import com.stefanbanu.todowebservices.services.TodoHardcodedService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoResource {
 
     private TodoHardcodedService todoHardcodedService;
